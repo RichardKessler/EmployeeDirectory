ac60c5baff854ef012d353fd6f499ce7c1d6254d
JidMenosta <jidmenosta@gmail.com>
2020-07-07 17:33:37 -0400

    added form and more components

---
+++ src/components/Form/indes.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+    state = {
+        name: ''
+    };
+
+    handleInputchange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+
+        let input, filter, table, tr, td, i, textValue;
+        input = document.getElementById('userInput');
+        filter = input.value.toUpperCase();
+        table = document.getElementById('table');
+        tr = table.getElementsByTagName('tr');
+
+        for (i = 0; i < tr.length; i++){
+            td = tr[i].getElementsByTagName('td')[0];
+            if (td){
+                textValue = td.textContent || td.innerText;
+                if (textValue.toUpperCase().indexOf(filter) > -1){
+                    tr[i].style.display = '';
+                } else {
+                    tr[i].style.display = 'none';
+                }
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <form className='form'>
+                    <input 
+                    id='userInput'
+                    vaule={this.state.name}
+                    name='name'
+                    onChange={this.handleInputchange}
+                    type='text'
+                    placeholder='Search'
+                    />
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Form;
