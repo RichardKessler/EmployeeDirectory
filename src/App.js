540106a3a194d6a8a33c824082cfd54337c2cd5e
JidMenosta <jidmenosta@gmail.com>
2020-07-08 16:36:43 -0400

    trying to get code to work

--- src/App.js
+++ src/App.js
@@ -14,9 +14,46 @@ class App extends Component {
     ascending: true
   };
 
-  nameSort = () => {
+  sortName = () => {
     const { sortedEmployees, ascending } = this.state
-    const 
+    const upOrDown = ascending ? 'asc' : 'desc'
+    const sorted = orderBy(sortedEmployees, 'name', upOrDown);
+
+    this.setState({ sortedEmployees: sorted, ascending: !ascending })
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <div className='container'>
+          <Title>Directory of Employees</Title>
+
+          <Form />
+          <table className='table'>
+            <thead>
+              <tr className='header'>
+                <th scope='col' onClick={this.sortName}>Name</th>
+                <th scope='col'>Phone Number</th>
+                <th scope='col'>Email</th>
+                <th scope='col'>Position</th>
+              </tr>
+            </thead>
+            {this.state.sortedEmployees.map(employee => {
+              return (
+                <Employee
+                id={employee.id}
+                key={employee.id}
+                name={employee.name}
+                phone={employee.phone}
+                email={employee.email}
+                position={employee.position}
+                />
+              )
+            })}
+          </table>
+        </div>
+      </Wrapper>
+    );
   }
 }
 
--- src/components/Employees/index.js
+++ src/components/Employees/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './style.css'
+
 
 let Employee = props => {
     return (
